39e648f5fd68ffe1da5a743b29e5c91a82092132
pawel <7proPS7@gmail.com>
2018-01-06 19:59:01 +0100

    Create more test in SaleTest class and add pay method. Set old admission method Deprecated and change abstract class with interface. Add new admission method.

--- src/test/java/car/analizer/util/innerclass/anonymous/SaleTest.java
+++ src/test/java/car/analizer/util/innerclass/anonymous/SaleTest.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SaleTest {
+    private final int BASE_PRICE = 20;
 
     @Test
     void checkAdmissionValue() {
-        final int BASE_PRICE = 20;
         Sale sale = new Sale();
         Assertions.assertEquals(10, sale.admission(BASE_PRICE));
     }
+
+    @Test
+    void checkPayMethodCorrectness() {
+        Sale sale = new Sale();
+        Assertions.assertEquals(15, sale.pay(BASE_PRICE));
+    }
+
 }
