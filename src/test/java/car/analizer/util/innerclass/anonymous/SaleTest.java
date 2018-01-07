37e9125dc56b760ec5148517070eda734f291a8a
pawel <7proPS7@gmail.com>
2018-01-07 15:41:11 +0100

    Create Run interface and it implementation inside Ostrich class. Create OstrichTest class

---
+++ src/test/java/car/analizer/util/innerclass/anonymous/SaleTest.java
@@ -0,0 +1,20 @@
+package car.analizer.util.innerclass.anonymous;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SaleTest {
+    private final int BASE_PRICE = 20;
+
+    @Test
+    void checkAdmissionValue() {
+        Sale sale = new Sale();
+        Assertions.assertEquals(10, sale.admission(BASE_PRICE));
+    }
+
+    @Test
+    void checkPayMethodCorrectness() {
+        Sale sale = new Sale();
+        Assertions.assertEquals(15, sale.pay(BASE_PRICE));
+    }
+}
