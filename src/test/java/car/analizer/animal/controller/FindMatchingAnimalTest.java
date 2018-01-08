8c8cb6ffc604eac2616c3670b85b2b6d72432059
pawel <7proPS7@gmail.com>
2018-01-08 20:44:15 +0100

    Add Predicate interface to FindMatchingAnimal class and add test with None skill example

--- src/test/java/car/analizer/animal/controller/FindMatchingAnimalTest.java
+++ src/test/java/car/analizer/animal/controller/FindMatchingAnimalTest.java
@@ -4,15 +4,23 @@ import car.analizer.animal.Animal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Predicate;
+
 class FindMatchingAnimalTest {
 
     @Test
     void checkIfAnimalCanDoSth() {
         Animal tiger = new Animal("TIGER", true, false);
-        Assertions.assertEquals(tiger.toString(), FindMatchingAnimal.print(tiger, Animal::canHop));
+        Assertions.assertEquals(tiger.toString(), FindMatchingAnimal.print(tiger, (Predicate<Animal>) Animal::canHop));
 
         Animal frog = new Animal("FROG", true, true);
-        Assertions.assertEquals(frog.toString(), FindMatchingAnimal.print(frog, Animal::canHop));
-        Assertions.assertEquals(frog.toString(), FindMatchingAnimal.print(frog, Animal::canSwim));
+        Assertions.assertEquals(frog.toString(), FindMatchingAnimal.print(frog, (Predicate<Animal>) Animal::canHop));
+        Assertions.assertEquals(frog.toString(), FindMatchingAnimal.print(frog, (Predicate<Animal>) Animal::canSwim));
+    }
+
+    @Test
+    void shouldReturnNoneIfAnimalOfNotSkillsExpected() {
+        Animal fakeAnimal = new Animal("FAKE_ANIMAL", false, false);
+        Assertions.assertEquals("NONE", FindMatchingAnimal.print(fakeAnimal, (Predicate<Animal>) Animal::canHop));
     }
 }
