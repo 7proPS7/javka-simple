42b31d78b4aedbc16d293562ff9d77a09c188b9d
Pawel S <7proPS7@gmail.com>
2018-01-14 21:14:40 +0100

    Revert "Create skeleton of reflection project. Analyzing Method example of re…"

--- src/main/java/com/props/tools/javkasimple/reflection/TextAnalyzer.java
+++
@@ -1,4 +0,0 @@
-package com.props.tools.javkasimple.reflection;
-
-public class TextAnalyzer {
-}
--- src/main/java/com/props/tools/javkasimple/reflection/TextLoader.java
+++
@@ -1,7 +0,0 @@
-package com.props.tools.javkasimple.reflection;
-
-public class TextLoader {
-    public static Class loadClass(String nameOfClass) throws ClassNotFoundException {
-        return Class.forName(nameOfClass);
-    }
-}
