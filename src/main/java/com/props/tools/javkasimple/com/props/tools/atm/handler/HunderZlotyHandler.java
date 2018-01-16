4a83e6880aafa12feaf3a8b75e92474d7deb354c
pawel <7proPS7@gmail.com>
2018-01-16 19:28:18 +0100

    Create skeleton of ATM Dispense Tool.

---
+++ src/main/java/com/props/tools/javkasimple/com/props/tools/atm/handler/HunderZlotyHandler.java
@@ -0,0 +1,16 @@
+package com.props.tools.javkasimple.com.props.tools.atm.handler;
+
+import com.props.tools.javkasimple.com.props.tools.atm.service.DispenseChain;
+import com.props.tools.javkasimple.com.props.tools.atm.util.Currency;
+
+public class HunderZlotyHandler implements DispenseChain {
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+
+    }
+
+    @Override
+    public void dispense(Currency currency) {
+
+    }
+}
