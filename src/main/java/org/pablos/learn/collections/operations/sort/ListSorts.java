396301960b826e1936c3d52b218365773ecec741
pawel <7proPS7@gmail.com>
2018-01-30 20:18:07 +0100

    Implementing Sort and Search class

---
+++ src/main/java/org/pablos/learn/collections/operations/sort/ListSorts.java
@@ -0,0 +1,14 @@
+package org.pablos.learn.collections.operations.sort;
+
+import java.util.Arrays;
+
+public class ListSorts {
+
+    void sortArray(int[] values) {
+        Arrays.sort(values);
+    }
+
+    void sortArray(String[] names) {
+        Arrays.sort(names);
+    }
+}
