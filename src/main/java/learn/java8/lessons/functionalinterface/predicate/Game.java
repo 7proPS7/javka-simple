61e71173d8a7160b140bdb0fae557ae177a21ffc
pawel <7proPS7@gmail.com>
2018-02-17 22:37:24 +0100

    Implementation of Description in PredicateClass and set Game class

--- src/main/java/learn/java8/lessons/functionalinterface/predicate/Game.java
+++ src/main/java/learn/java8/lessons/functionalinterface/predicate/Game.java
@@ -3,6 +3,7 @@ package learn.java8.lessons.functionalinterface.predicate;
 public class Game {
     private String name;
     private String author;
+    private String description;
 
     public String getName() {
         return name;
@@ -18,5 +19,13 @@ public class Game {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
